84b31b1092a2d6eb00b58e719530bbc804735b9c
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-23 23:56:05 -0500

    feat: add total prices to cart

--- src/app/(shop)/cart/components/price-resume.tsx
+++ src/app/(shop)/cart/components/price-resume.tsx
@@ -5,23 +5,33 @@ export default function PriceResume() {
     items: state.items,
   }));
 
+  const subtotal = items.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
+
+  const totalDiscount = items.reduce((acc, item) => {
+    return acc + ((item.price * item.discount) / 100) * item.quantity;
+  }, 0);
+
+  const total = subtotal - totalDiscount;
+
   return (
     <div className="border border-gray-200 rounded p-6 xl:w-[420px]">
       <h2 className="mb-6 text-xl font-medium">Card Totals</h2>
       <div className="space-y-4">
         <div className="flex justify-between text-sm">
           <span className="text-gray-600">Sub-total</span>
-          <span className="font-medium">$320</span>
+          <span className="font-medium">${subtotal.toFixed(2)}</span>
         </div>
         <div className="flex justify-between text-sm">
           <span className="text-gray-600">Discount</span>
-          <span className="font-medium">$24</span>
+          <span className="font-medium">${totalDiscount.toFixed(2)}</span>
         </div>
         <div className="border-t border-gray-200 pt-4">
           <div className="flex justify-between">
             <span className="text-gray-600">Total</span>
             <div className="text-right">
-              <div className="text-lg font-medium">$357.99 USD</div>
+              <div className="text-lg font-medium">${total.toFixed(2)} USD</div>
             </div>
           </div>
         </div>
